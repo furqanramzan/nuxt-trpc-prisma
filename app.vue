4eaf3667f04d1acc62be35e13ed66794b69555c7
Muhammad Furqan <furqanramzan271996@gmail.com>
2023-02-13 01:07:41 +0500

    Add on mounted in api call

--- app.vue
+++ app.vue
@@ -1,9 +1,13 @@
 <script setup>
-const { data: hello } = await useFetch("/api/hello");
+let hello = ref("");
+onMounted(async () => {
+  hello.value = await $fetch("/api/hello");
+});
 </script>
 
 <template>
   <div>
-    <h1>{{ hello }}</h1>
+    <h1 v-if="hello">{{ hello }}</h1>
+    <h1 v-else>Loading</h1>
   </div>
 </template>
